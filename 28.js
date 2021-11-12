7dd13aec96440fb003914b120e8efaabd94fedd6
Angrmz <66383546+Angrmz@users.noreply.github.com>
2021-11-11 17:14:57 -0700

    Se crea archivo 28.js
    28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.

---
+++ 28.js
@@ -0,0 +1,13 @@
+var num = Random(1, 2)
+var n1 = 1;
+var n2 = 2;
+
+function Random(n1, n2) {
+  return Math.random() * (2 - 1) + 1;
+}
+
+if(num == 1){
+	console.log("Cruz");
+}else{
+	console.log("Cruz");
+}
