7af220f8954fe2b10d04ae58ed6b5b5f964fd1eb
Angrmz <66383546+Angrmz@users.noreply.github.com>
2021-11-14 13:21:58 -0700

    Se crea archivo 32.js
    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista 
    debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,5 @@
+const arreglo = ["Angelica","Andres","Gustavo","Manuel","Angel","Antonio","Dante","Nubya","Lluvia","Israel","Brayan","Erick","Fernando","Alma","Alejandra","Yahir","Michelle","Sabrina","Carmen","Cruz"];
+
+for (var i = 0; i < arreglo.length; i++) {
+  console.log(arreglo[i]);
+}
